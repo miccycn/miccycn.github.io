1d31c05f257e39c0825207f75d03795e3e622c77
weiyuanyue <millywey@qq.com>
2017-06-08 16:23:27 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -70,6 +70,24 @@ self.addEventListener('activate', function(e) {
   return self.clients.claim();
 });
 
+window.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
+
+  // e.userChoice will return a Promise.
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
+
 self.addEventListener('fetch', function(e) {
   // console.log('[Service Worker] Fetch', e.request.url);
   var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
